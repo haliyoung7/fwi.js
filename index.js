f934cf1aa4642541bbb87cc6c9327a6559c47b4d
Erin O'Connell <erinocon5@gmail.com>
2017-10-10 00:09:33 -0600

    added the libs!

---
+++ index.js
@@ -0,0 +1,5 @@
+import { Player } from './libs/player.js';
+import { Template } from './libs/template.js';
+import { Content } from './libs/content.js';
+import { Region } from './libs/region.js';
+import { Helpers } from './libs/helpers.js';
---
+++ libs/helpers.js
@@ -0,0 +1,23 @@
+import { Player } from './player';
+
+export class Helpers {
+
+    constructor() {
+
+    }
+
+    static poller(var_name, callback, timeout = 50) {
+
+        if (Player.GetVariable(var_name) != 'Undefined') {
+            callback();
+            Player.SetVariable(var_name, 'Undefined');
+        };
+
+        const timeout_function = () => {
+            Helpers.poller(var_name, callback, timeout);
+        }
+
+        window.setTimeout(timeout_function, timeout);
+
+    }
+}
