457b586a680913e6e12af8237b3cfd5d5a258b01
Erin O'Connell <erinocon5@gmail.com>
2017-10-10 01:32:18 -0600

    fd

---
+++ index.js
@@ -0,0 +1,4 @@
+const Player = require('./libs/player.js');
+const Template = require('./libs/template.js');
+const Content = require('./libs/content.js');
+const Region = require('./libs/region.js');
