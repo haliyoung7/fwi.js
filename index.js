cc07843c0d7db68ba99bafbf9318a13a36a64bec
Erin O'Connell <erinocon5@gmail.com>
2017-10-10 01:03:25 -0600

    let's see if this works

--- index.js
+++
@@ -1,5 +0,0 @@
-import { Player } from './libs/player.js';
-import { Template } from './libs/template.js';
-import { Content } from './libs/content.js';
-import { Region } from './libs/region.js';
-import { Helpers } from './libs/helpers.js';
