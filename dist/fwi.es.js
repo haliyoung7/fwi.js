81e21b7cd057c77ef77250ce3361add72a67d54b
Erin O'Connell <erinocon5@gmail.com>
2017-10-15 00:17:36 -0600

    corrected relative path

--- dist/fwi.es.js
+++ dist/fwi.es.js
@@ -1,4 +1,4 @@
-import { Player } from "./src/player.js";
-import { Template } from "./src/template.js";
-import { Content } from "./src/content.js";
-import { Region } from "./src/region.js";
+import { Player } from "../src/player.js";
+import { Template } from "../src/template.js";
+import { Content } from "../src/content.js";
+import { Region } from "../src/region.js";
