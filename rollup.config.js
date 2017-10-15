799abd4013b3b7a52edac66a53b269b76c7b5ce0
Erin O'Connell <erinocon5@gmail.com>
2017-10-15 00:08:56 -0600

    renamed a file

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default {
   input: './index.js',
   output: [
       {
-          file: 'dist/fwi.es.js',
-          format: 'es'
+          file: 'dist/fwi.browser.js',
+          format: 'iife'
       },
   ],
   plugins: [
-      resolve()
+      resolve(),
+      babel()
   ]
 }
